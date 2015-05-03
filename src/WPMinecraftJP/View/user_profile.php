25d2f537b66a2e86a0e8239f0e03d71ced1b13f6
ayunyan <ayu@commun.jp>
2015-05-04 08:58:38 +0900

    Added minecraft username display

--- src/WPMinecraftJP/View/user_profile.php
+++ src/WPMinecraftJP/View/user_profile.php
@@ -4,9 +4,11 @@
         <th><?php echo __('minecraft.jp', WPMinecraftJP\App::NAME); ?></th>
         <td>
             <?php if ($isLinked): ?>
-                <a href="<?php echo home_url('/minecraftjp/unlink?token=' . wp_create_nonce('minecraftjp_unlink')); ?>" class="button button-secondary"><?php echo __('Unlink account', WPMinecraftJP\App::NAME); ?></a>
+                <p class="description"><?php echo __('Minecraft Username', WPMinecraftJP\App::NAME); ?>: <?php echo get_user_meta(get_current_user_id(), 'minecraftjp_username', true); ?></p>
+
+                <p><a href="<?php echo home_url('/minecraftjp/unlink?token=' . wp_create_nonce('minecraftjp_unlink')); ?>" class="button button-secondary"><?php echo __('Unlink account', WPMinecraftJP\App::NAME); ?></a></p>
             <?php else: ?>
-                <a href="<?php echo home_url('/minecraftjp/login?type=link'); ?>" class="button button-secondary"><?php echo __('Link account', WPMinecraftJP\App::NAME); ?></a>
+                <p><a href="<?php echo home_url('/minecraftjp/login?type=link'); ?>" class="button button-secondary"><?php echo __('Link account', WPMinecraftJP\App::NAME); ?></a></p>
             <?php endif; ?>
         </td>
     </tr>
